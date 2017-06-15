830298baa2aaa1600d31bc6cf3d36e1a3f74d8c5
Azowyl <facugerstner_29@hotmail.com>
2017-06-15 12:05:30 -0300

    toques finales

--- Duration_Prediction_algorithm.py
+++ Duration_Prediction_algorithm.py
@@ -45,7 +45,7 @@ def preprocess_data(stations, weather, train_trips, test_trips):
     train_data.drop(labels=['id', 'start_station_name', 'end_station_name', 'date', 'zip_code', 'bike_id'], axis=1, inplace=True)
 
     test_data = pd.merge(test_trips, weather, on=['date', 'city_zip_code'], how='inner')
-    test_data.drop(labels=['id', 'start_station_name', 'end_station_name', 'date', 'zip_code', 'bike_id'], axis=1, inplace=True)
+    test_data.drop(labels=['start_station_name', 'end_station_name', 'date', 'zip_code', 'bike_id'], axis=1, inplace=True)
 
     return train_data, test_data
 
@@ -69,17 +69,13 @@ def the_Hashing_Trick(data, column_labels):
     return data
 
 
-def data_standarization(train_data, test_data = None):
+def data_normalization(train_data, test_data):
 
-    scaler = preprocessing.StandardScaler().fit(train_data)
+    data = (train_data - train_data.mean()) / (train_data.max() - train_data.min())
 
-    data = scaler.transform(train_data)
-
-    if (test_data != None):
-        test_data = scaler.transform(test_data)
-        return data, test_data
-
-    return data
+    t_data = (test_data - test_data.mean()) / (test_data.max() - test_data.min())
+    
+    return data, t_data
 
 
 def reduce_dimentions_to(data, n_dimensions = 5):
@@ -89,27 +85,39 @@ def reduce_dimentions_to(data, n_dimensions = 5):
     return svd.fit_transform(data)
 
 
-def impute_values(data, strategy):
+def impute_nan(data, strategy):
 
     return preprocessing.Imputer(missing_values='NaN', strategy=strategy, axis=0).fit_transform(data)
 
 
-def train_model(x, y):
-    return BayesianRidge(lambda_1=1.e-6, lambda_2=1.e-6, noramilize=True).fit(x, y)
+def train_bayesian_model(x, y):
+
+    bayesian_regression = BayesianRidge(lambda_1=1.e-9, lambda_2=1.e-10)
+
+    return BaggingRegressor(bayesian_regression, n_estimators=20, max_samples=0.5, n_jobs=-1).fit(x, y)
+
+
+def train_linear_model(x, y):
+
+    linear_regression = LinearRegression(fit_intercept=False,n_jobs=-1)
+
+    return BaggingRegressor(linear_regression, n_estimators=20, max_samples=1.0, n_jobs=-1).fit(x, y)
+
+start = time()
 
 
 #input
 stations = pd.read_csv('data/station.csv')
-trips = pd.read_csv('data/trip.csv', parse_dates=['start_date', 'end_date'], infer_datetime_format=True,low_memory=False)
 weather = pd.read_csv('data/weather.csv', parse_dates=['date'], infer_datetime_format=True)
 cities = pd.read_csv('data/cities.csv')
 
-trip_train = pd.read_csv('data/trip_train.csv')
-trip_test = pd.read_csv('data/trip_test.csv')
+trip_train = pd.read_csv('data/trip_train.csv',  parse_dates=['start_date', 'end_date'], infer_datetime_format=True,low_memory=False)
+trip_test = pd.read_csv('data/trip_test.csv',  parse_dates=['start_date', 'end_date'], infer_datetime_format=True,low_memory=False)
 
-
+# Pre-processing
 train_set, test_set = preprocess_data(stations, weather, trip_train, trip_test)
 
+# Feature Hashing
 column_labels = ['start_station_id','end_station_id','subscription_type','city_zip_code','events']
 train_set = the_Hashing_Trick(train_set, column_labels)
 test_set = the_Hashing_Trick(test_set, column_labels)
@@ -118,14 +126,34 @@ target_values = train_set['duration']
 train_set.drop(labels='duration', axis=1, inplace=True)
 
 ids = test_set['id']
-trip_test.drop(labels=['id'], axis=1, inplace=True)
+test_set.drop(labels=['id'], axis=1, inplace=True)
 
-train_set = impute_values(train_set)
-test_set = impute_values(test_set)
+# NaN Imputation
+train_set = impute_nan(train_set, 'most_frequent')
+test_set = impute_nan(test_set, 'most_frequent')
 
-regressor = train_model(train_set, target_values)
+# Data Normalization
+train_set_normalized, test_set_normalized = data_normalization(train_set, test_set)
 
-results = pd.DataFrame(data=regressor.predict(trip_test), index=ids).reset_index(inplace=True)
-results.columns = ['id', 'duration']
+# Training
+bayesian_regressor = train_bayesian_model(train_set, target_values)
+linear_regressor = train_linear_model(train_set_normalized, target_values)
+
+# Predictions
+bayesian_pred = bayesian_regressor.predict(test_set)
+bayesian_results = pd.DataFrame(data=bayesian_pred, index=ids)
+bayesian_results.reset_index(inplace=True)
+bayesian_results.columns = ['id', 'duration']
+
+linear_pred = linear_regressor.predict(test_set_normalized)
+linear_results = pd.DataFrame(data=linear_pred, index=ids)
+linear_results.reset_index(inplace=True)
+linear_results.columns = ['id', 'duration']
+
+results = pd.merge(linear_results, bayesian_results, on='id')
+results['duration'] = pd.DataFrame(data=results.loc[:, ['duration_x','duration_y']].mean(axis=1))
+results.drop(labels=['duration_x','duration_y'], axis=1, inplace=True)
 
 results.to_csv('results.csv', index=False)
+
+print ("Prediction took %.2f seconds", time() - start)
